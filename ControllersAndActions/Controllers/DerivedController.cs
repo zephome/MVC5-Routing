cdcabaedb7428cadc7b3c49407e88ac15b9e0ecd
zephome@gmail.com <zeppelin78>
2016-09-20 17:39:52 +0900

    컨트롤과 액션 (p.583)

---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,34 @@
+﻿using ControllersAndActions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            if (Server.MachineName == "개발_김형곤")
+            {
+                //Response.Redirect("/Basic/Index");
+                //return new CustomRedirectResult { Url = "/Basic/Index" };
+                return new RedirectResult("/Basic/Index");
+            }
+            else
+            {
+                //Response.Write("Controller: Derived, Action: ProduceOutput");
+                return null;
+            }
+        }
+    }
+}
---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ViewResult Index()
+        {
+            DateTime date = DateTime.Now;
+
+            ViewBag.Message = "Hello";
+            ViewBag.Date = DateTime.Now;
+
+            return View();
+        }
+
+        public RedirectToRouteResult Redirect()
+        {
+            Random random = new Random();
+
+            TempData["Message"] = random.Next(0, 100).ToString();
+            TempData["Date"] = DateTime.Now;
+
+            return RedirectToAction("Index", "Example", new { id = "MyID" });
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            //return new HttpStatusCodeResult(404, "URL can't be serviced");
+            //return HttpNotFound();
+            return new HttpUnauthorizedResult();
+        }
+    }
+}
