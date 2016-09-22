e2ff2ebda6bc4ae15e651bbc24bd6e85db8f6e0d
김형곤 <zephome@gmail.com>
2016-09-22 18:09:33 +0900

    레이아웃 섹션 (p.744)

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string[] names = { "Apple", "Orange", "Pear" };
+
+            return View(names);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
