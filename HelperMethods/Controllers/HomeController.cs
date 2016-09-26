0e814e0981c16b50cf429585861d696e65e6e50b
김형곤 <zephome@gmail.com>
2016-09-26 14:45:37 +0900

    헬퍼메서드 (p.764)

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Apple", "Banana", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            string message = "This is a HTML Element: <input>";
+
+            return View((object)message);
+        }
+    }
+}
