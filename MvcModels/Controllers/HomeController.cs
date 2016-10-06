b7339dd0aa5c0855f9a61ebdfad6cd4afa6ee9f8
김형곤 <zephome@gmail.com>
2016-10-06 17:58:37 +0900

    컨트롤러 생성 (p.879)

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MvcModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] personData =
+        {
+            new Person { FirstName = "Adam", LastName = "Freeman", Role = Role.Admin },
+            new Person { FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User },
+            new Person { FirstName = "John", LastName = "Smith", Role = Role.User },
+            new Person { FirstName = "Anne", LastName = "Jones", Role = Role.User }
+        };
+
+        // GET: Home
+        public ActionResult Index(int id)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+
+            return View(dataItem);
+        }
+    }
+}
