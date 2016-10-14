777f274ff701611b4f47a42bfc2d8381e68e68bd
김형곤 <zephome@gmail.com>
2016-10-14 17:39:20 +0900

    예제 프로젝트 생성 (p.918)

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            return View("Complete", appt);
+        }
+    }
+}
