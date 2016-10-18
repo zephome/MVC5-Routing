82d2df81b437c7094819e46851d1514485ee4436
김형곤 <zephome@gmail.com>
2016-10-18 17:15:39 +0900

    번들 (p.959)

---
+++ ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using ClientFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Adam",
+                TermsAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ClientFeatures/Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientFeatures.Models
+{
+    public class Appointment
+    {
+        public string ClientName { get; set; }
+
+        public bool TermsAccepted { get; set; }
+    }
+}
